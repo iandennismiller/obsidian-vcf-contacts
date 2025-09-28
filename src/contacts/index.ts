0e3c053a581f0fa73eb9b6de55b47858e7a5f82e
Ian Dennis Miller <ian@iandennismiller.com>
2025-09-28 12:51:16 -0400

    clean up

--- src/contacts/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './contactNote';
-export * from './contactManager';
-export * from './vcardManager';
-
-// Re-export utility functions
-export { getFrontmatterFromFiles, parseKey, mdRender } from './contactNote';
